03542fc83b020b462da9611b9c1da848980f2f5e
enoch232 <enoch232@gmail.com>
2016-10-01 22:58:34 -0700

    overwrite old files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 const port = process.env.PORT || 3000
-console.log("Server is now running at "+ port)
+console.log("Server is now running at port "+ port)
 app.listen(3000)
 app.get("/", (req, res)=>{
 	res.end("hello world!")
