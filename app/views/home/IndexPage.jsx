2e1458769c59616d0ce5b2e483145e3ba955b652
Samson Ko <samsonko210@gmail.com>
2016-10-15 15:44:36 -0700

    removed 5 categories

--- app/views/home/IndexPage.jsx
+++ app/views/home/IndexPage.jsx
@@ -12,7 +12,7 @@ class IndexPage extends React.Component{
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png" />
         <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
         <link rel="shortcut icon" href="assets/ico/favicon.png" />
-        <title>CollegeStuffSale</title>
+        <title>CollegeStuffSaleNODE</title>
         {/* Bootstrap core CSS */}
         <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
         {/* Custom styles for this template */}
--- app/views/home/categories.jsx
+++ app/views/home/categories.jsx
@@ -14,78 +14,33 @@ class Category extends React.Component {
 	          Category <a href="category.html" className="sell-your-item"> View more <i className="icon-th-list"></i> </a></h2>
 	        </div>
 	      </div>
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-	        <a href="category.html"><img src="/assets/img/category/cars.jpg" className="img-responsive" alt="img"/>
-	          <h6> Automobiles </h6>
-	        </a>
-	      </div>
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/laptops.png" className="img-responsive" alt="img"/>
-          	<h6> Computers & Tablets</h6>
-          </a>
-	      </div>
 
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
+	      <div className="col-md-4 col-md-3 col-sm-3 col-xs-4 f-category">
           <a href="category.html"><img src="/assets/img/category/cellphones.png" className="img-responsive" alt="img"/>
             <h6> Cellphones</h6>
           </a>
 	      </div>
 
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
+	      <div className="col-md-4 col-md-3 col-sm-3 col-xs-4 f-category">
           <a href="category.html"><img src="/assets/img/category/applewatch.png" className="img-responsive" alt="img"/>
           	<h6>Electronics </h6>
           </a>
 	      </div>
 
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
+	      <div className="col-md-4 col-md-3 col-sm-3 col-xs-4 ">
           <a href="category.html">
-          	<img src="/assets/img/category/games.png" className="img-responsive" alt="img"/>
-            <h6> Movies & Games </h6>
-          </a>
-	      </div>
-
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html">
-          	<img src="/assets/img/category/textbooks.jpg" className="img-responsive" alt="img"/>
+          	<img src="/assets/img/category/textbookcarry.jpg" className="img-responsive" alt="img"/>
             <h6> Textbooks </h6>
           </a>
 	      </div>
 
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
+	      <div className="col-md-4 col-md-3 col-sm-3 col-xs-4 f-category">
 	        <a href="category.html"><img src="/assets/img/category/home .png" className="img-responsive" alt="img"/>
 	        	<h6> Home </h6>
 	        </a>
 	      </div>
 
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/camera.jpg" className="img-responsive" alt="img"/>
-          	<h6> Cameras </h6>
-        	</a>
-	      </div>
-
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/makeup.jpg" className="img-responsive" alt="img"/>
-            <h6> Beauty </h6>
-          </a>
-	      </div>
-
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/friends.png" className="img-responsive" alt="img"/>
-              <h6> Roommates </h6>
-          </a>
-	      </div>
-
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/jobs.png" className="img-responsive" alt="img"/>
-              <h6> Jobs </h6>
-          </a>
-	      </div>
-
-	      <div className="col-lg-2 col-md-3 col-sm-3 col-xs-4 f-category">
-          <a href="category.html"><img src="/assets/img/category/clothing.png" className="img-responsive" alt="img"/>
-          	<h6> Clothing & Fashion </h6>
-          </a>
-	      </div>
+	     
       </div>
     )
   }
